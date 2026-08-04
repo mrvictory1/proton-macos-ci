57203ae2906b561af32d33c7bc1d99fb53ff31c3
mrvictory1 <mrvictory1@protonmail.com>
2026-08-04 15:06:05 +0300

    build-proton.sh: Clone with --depth=1 on GitHub Actions

--- build-proton.sh
+++ build-proton.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [[ -v GITHUB_ACTIONS ]]; then
+DEPTH="--depth=1"
+fi
+
 if [[ ! $(cat .brew-deps) -gt 0 ]] ; then
 brew install automake bison cmake coreutils findutils fontforge glib glib-networking grep libnghttp2 lld make meson mingw-w64 molten-vk mpg123 nasm opus orc rust sdl2-compat spirv-headers theora vulkan-headers wget
 echo "1" > .brew-deps
@@ -22,14 +26,14 @@ export PATH="/usr/local/opt/findutils/libexec/gnubin:$PATH"
 export PATH="/usr/local/opt/make/libexec/gnubin:$PATH"
 export PATH="/usr/local/opt/libtool/libexec/gnubin:$PATH"
 
-git clone https://github.com/ValveSoftware/Proton/
+git clone https://github.com/ValveSoftware/Proton/ $DEPTH
 cd Proton
 
 # Upstream DXVK is not compatible with macOS
 # git am ../patches/repo-pre/*
 git submodule set-url dxvk https://github.com/Gcenx/DXVK-macOS
 git submodule set-branch --branch 1.10.x dxvk
-git submodule update --init --recursive
+git submodule update --init --recursive $DEPTH
 git am ../patches/repo/*
 
 to_patch=("piper" "wine")
