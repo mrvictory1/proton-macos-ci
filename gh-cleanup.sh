6b2317455c33148520dc1d6e300a77b2702235db
mrvictory1 <108172910+mrvictory1@users.noreply.github.com>
2026-08-05 10:56:48 +0300

    gh-cleanup.sh: Delete simulator runtimes instead of Xcode

--- gh-cleanup.sh
+++ gh-cleanup.sh
@@ -2,12 +2,14 @@
 # Clear up disk space on GitHub Actions
 
 sudo rm -rf /Users/runner/Library/Android
-sudo rm -rf /Applications/Xcode_16.app \
-  /Applications/Xcode_16.1.app \
-  /Applications/Xcode_16.2.app \
-  /Applications/Xcode_16.3.app \
-  /Applications/Xcode_26.0.1.app \
-  /Applications/Xcode_26.1.1.app \
-  /Applications/Xcode_26.2.app \
-  /Applications/Xcode_26.3.app
-
+xcrun simctl runtime delete all
+: '
+sudo rm -rf /Applications/Xcode_16.app
+sudo rm -rf /Applications/Xcode_16.1.app
+sudo rm -rf /Applications/Xcode_16.2.app
+sudo rm -rf /Applications/Xcode_16.3.app
+sudo rm -rf /Applications/Xcode_26.0.1.app
+sudo rm -rf /Applications/Xcode_26.1.1.app
+sudo rm -rf /Applications/Xcode_26.2.app
+sudo rm -rf /Applications/Xcode_26.3.app
+'
