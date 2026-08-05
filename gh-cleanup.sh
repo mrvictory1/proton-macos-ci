5f3c6375a18c2e2d8f20ab02db7c1f90464021cc
mrvictory1 <108172910+mrvictory1@users.noreply.github.com>
2026-08-05 10:47:09 +0300

    Update gh-cleanup.sh

--- gh-cleanup.sh
+++ gh-cleanup.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 # Clear up disk space on GitHub Actions
-# Source: https://github.com/CesiumGS/cesium-unreal/blob/main/.github/workflows/buildApple.yml
 
-brew uninstall google-chrome
 sudo rm -rf /Users/runner/Library/Android
-sudo rm -rf /Applications/Xcode_14.3.app
-sudo rm -rf /Applications/Xcode_14.3.1.app
-sudo rm -rf /Applications/Xcode_15.0.1.app
-sudo rm -rf /Applications/Xcode_15.0.app
-sudo rm -rf /Applications/Xcode_15.1.0.app
-sudo rm -rf /Applications/Xcode_15.1.app
-sudo rm -rf /Applications/Xcode_15.2.0.app
-sudo rm -rf /Applications/Xcode_15.2.app
-sudo rm -rf /Applications/Xcode_15.3.0.app
-sudo rm -rf /Applications/Xcode_15.3.app
-sudo rm -rf /Applications/Xcode_15.4.0.app
-sudo rm -rf /Applications/Xcode_15.4.app
-sudo rm -rf /Applications/Xcode_16.0.0.app
-sudo rm -rf /Applications/Xcode_16.0.app
+sudo rm -rf /Applications/Xcode_16.app \
+  /Applications/Xcode_16.1.app \
+  /Applications/Xcode_16.2.app \
+  /Applications/Xcode_16.3.app \
+  /Applications/Xcode_26.0.1.app \
+  /Applications/Xcode_26.1.1.app \
+  /Applications/Xcode_26.2.app \
+  /Applications/Xcode_26.3.app
 
