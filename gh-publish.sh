d954743ddf57388bab8ba8745d7d0bfbf3df8431
mrvictory1 <108172910+mrvictory1@users.noreply.github.com>
2026-08-07 23:05:31 +0300

    Update gh-publish.sh

--- gh-publish.sh
+++ gh-publish.sh
@@ -2,5 +2,5 @@
 ZIP_FILE=Proton-$(date +%d%m%y)-macOS-x86_64.zip
 cd Proton/build/build*/redist/
 zip -ry $ZIP_FILE .
-gh release create -t "Release-$(date '+%d-%b-%y')" --notes "Release $(date +%d %b %y)" $ZIP_FILE
+gh release create -t "Release-$(date '+%d-%b-%y')" --notes "Release $(date '+%d %b %y')" $ZIP_FILE
 cd -
