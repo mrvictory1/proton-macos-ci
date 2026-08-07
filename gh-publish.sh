db45ee863d2f8abec221474813ab4ea0beb1e79c
mrvictory1 <108172910+mrvictory1@users.noreply.github.com>
2026-08-07 16:55:17 +0300

    gh-publish.sh: don't follow symlinks when creating zip file
    On local builds, the default prefix has no symlinks but on GitHub it has dosdevices/z: which links to root (/) and zip happily attemps to compress the entire OS.

--- gh-publish.sh
+++ gh-publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ZIP_FILE=Proton-$(date +%d%m%y)-macOS-x86_64.zip
 cd Proton/build/build*/redist/
-zip -r $ZIP_FILE .
+zip -ry $ZIP_FILE .
 gh release create -t "Release-$(date '+%d-%b-%y')" --notes "Release $(date +%d %b %y)" $ZIP_FILE
 cd -
