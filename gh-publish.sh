470366cf82f607ffd6dde03db342ef0cd96ab9e0
mrvictory1 <mrvictory1@protonmail.com>
2026-08-08 15:39:02 +0300

    gh-publish.sh: specify tag of publish

--- gh-publish.sh
+++ gh-publish.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -e
 ZIP_FILE=Proton-$(date +%d%m%y)-macOS-x86_64.zip
 cd Proton/build/build*/redist/
 zip -ry $ZIP_FILE .
 cd -
-gh release create -t "Release-$(date '+%d-%b-%y')" --notes "Release $(date '+%d %b %y')" Proton/build/build*/redist/$ZIP_FILE
+gh release create $(date '+%d-%b-%y') -t "Release-$(date '+%d-%b-%y')" --notes "Release $(date '+%d %b %y')" Proton/build/build*/redist/$ZIP_FILE
